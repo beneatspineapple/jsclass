d96fd17af740549bd87a9bf4de38e3df94e6435d
beneatspineapple <38222021+beneatspineapple@users.noreply.github.com>
2020-01-18 14:34:25 -0800

    Update js.js

--- js.js
+++ js.js
@@ -1,51 +1,60 @@
-console.log('Hi there!');
+// console.log('Hi there!');
 // Prints: Hi there!
 
-//====================================================
+// ====================================================
+// String data types
 // let single = 'Wheres my bandit hat?';
 // let double = "Wheres my bandit hat?";
 // console.log(single);
 // console.log(double);
-
 //====================================================
+// Number data types
+// This is an integer
 // let x = 2;
+// This is a floating point
 // let y = 2.00;
-
-
-//====================================================
-// let lateToWork = true;
+// console.log(x+y);
 //
+
+// ====================================================
+// Boolean data type
 // let lateToWork = true;
-
+// lateToWork = false;
+// console.log(lateToWork);
 
 
 //====================================================
-// // Addition
-// 5 + 5
+// Each of these are called operators
+// Addition
+// console.log(5 + 5);
 // // Subtraction
-// 10 - 5
+// console.log(10 - 5);
 // // Multiplication
-// 5 * 10
+// console.log(5 * 10);
 // // Division
-// 10 / 5
+// console.log(10 / 5);
 // // Modulo
-// 10 % 5
+// // Modulo is remainder division
+// console.log(9 % 5);
 //====================================================
 
-
+// Green strings are called literals
+// You could also call x a string, but it's not a literals
+// x is a string variable
 // let x = 'good nite~';
 // console.log(x.length);
 // // Expected output: 10
 //
+// //
 // console.log('howdy'.length);
 // // Expected output: 5
 //====================================================
 
 
-
-// Returns a number between 0 and 1.
-// Math.random();
-//====================================================
+// let x =Math.random();
+// // Returns a number between 0 and 1.
+// console.log(x);
+// //====================================================
 
 
 // Math.random();
@@ -103,12 +112,13 @@ console.log('Hi there!');
 
 
 
-// let service = 'credit card';
-// let month = 'May 30th';
-// let displayText = 'Your ' + service + ' bill is due on ' + month + '.';
-//
-// console.log(displayText);
-// // output: Your credit card bill is due on May 30th.
+let service = 'credit card';
+let month = 'May 30th';
+let displayText = 'Your ' + service + ' bill is due on ' + month + '.';
+// Gluing strings together is concatenating (+)
+
+console.log(displayText);
+// output: Your credit card bill is due on May 30th.
 //====================================================
 
 
