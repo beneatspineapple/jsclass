503db931c5e22250dffecb7703786def5b5a6241
Shawn K <shawnkilmer@gmail.com>
2020-01-18 13:25:10 -0800

    initial checkin of class2 files

---
+++ js.js
@@ -0,0 +1,131 @@
+console.log('Hi there!');
+// Prints: Hi there!
+
+//====================================================
+// let single = 'Wheres my bandit hat?';
+// let double = "Wheres my bandit hat?";
+// console.log(single);
+// console.log(double);
+
+//====================================================
+// let x = 2;
+// let y = 2.00;
+
+
+//====================================================
+// let lateToWork = true;
+//
+// let lateToWork = true;
+
+
+
+//====================================================
+// // Addition
+// 5 + 5
+// // Subtraction
+// 10 - 5
+// // Multiplication
+// 5 * 10
+// // Division
+// 10 / 5
+// // Modulo
+// 10 % 5
+//====================================================
+
+
+// let x = 'good nite~';
+// console.log(x.length);
+// // Expected output: 10
+//
+// console.log('howdy'.length);
+// // Expected output: 5
+//====================================================
+
+
+
+// Returns a number between 0 and 1.
+// Math.random();
+//====================================================
+
+
+// Math.random();
+// // ☝️ Math is the library
+//====================================================
+
+// console.log(Math.random());
+// // Output: 0 - 0.9
+//====================================================
+
+
+
+// let number = 15.95;
+// console.log(Math.floor(number)); // 15
+
+// number = -99.5;
+// console.log(Math.floor(number)); // -100
+//====================================================
+
+
+// const numberOfColumns = 4;
+// numberOfColumns = 8;
+// // TypeError: Assignment to constant variable.
+//====================================================
+
+
+
+
+// let count;
+// console.log(count); // Logs undefined in console
+// count = 10;
+// console.log(count); // Logs 10 in console
+//====================================================
+
+
+
+
+
+// var a;
+// console.log(a); // undefined
+// let b;
+// console.log(b); // undefined
+//====================================================
+
+// let number = 100;
+// // Both statements will add 10 to the number
+// number = number + 10;
+// number += 10;
+//
+// console.log(number); // Output: 120
+//====================================================
+
+
+
+
+
+
+// let service = 'credit card';
+// let month = 'May 30th';
+// let displayText = 'Your ' + service + ' bill is due on ' + month + '.';
+//
+// console.log(displayText);
+// // output: Your credit card bill is due on May 30th.
+//====================================================
+
+
+
+
+
+
+
+// const incomeCurrency = '$';
+// let userIncome = 85000;
+//
+// console.log(incomeCurrency + userIncome + ' is more than the average income.');
+// // Prints: $85000 is more than the average income.
+//====================================================
+
+
+
+// var age;
+// let weight;
+// const numberOfFingers = 20;
