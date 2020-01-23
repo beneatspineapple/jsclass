8751e2dc218dfb128932a506019f7c1e54b9c688
Shawn K <shawnkilmer@gmail.com>
2020-01-23 09:36:41 -0800

    added js2.js conditional examples

---
+++ js2.js
@@ -0,0 +1,45 @@
+// Boolean data type variables
+let lateToWork = true;
+let hasSnacks = true;
+let isRaining = false;
+let isWednesday = true;
+let gettingOutEarly = false;
+
+// integer data type variables
+let myAge = 37;
+
+// string data type
+let myBossesName = 'Sam';
+
+
+
+// exercises: write an if-statement for each of the following, using the above variables.
+
+// example: "write an if-statement that outputs something to the console if it's wednesday and you're late to work"
+if (isWednesday && lateToWork) {
+  console.log("it is wednesday, and you're late to work!");
+}
+//=================================
+
+// exercise 1. "write an if-statement that outputs something to the console if you are late to work and have snakcs"
+// if(){
+//
+// }
+//=================================
+
+
+//exercise 2. write an if-statement that outputs something when its either wednesday or it's raining
+
+
+
+//=================================
+
+
+//exercise 3. write an if-statement that outputs something when its NOT raining
+
+
+
+//=================================
+
+
+//exercise 4. write an if-statement that outputs "You are old!" if myAge is greater than 37, otherwise it outputs "you're still young!"
