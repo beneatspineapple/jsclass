1855a0a12a0b82faa5a1fd9d77b4e98d642c498a
beneatspineapple <38222021+beneatspineapple@users.noreply.github.com>
2020-02-10 21:28:28 -0800

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -6,17 +6,20 @@ let userInputtedRPSString = prompt("type your choice: rock, paper, or scissors")
 let randomFloatingPointNumber = Math.random() * 100;
 
 // a variable to hold a string representing the computer opponents choice
-let computersChoice = '';
+// let computersChoice = '';
 // =========== leave above section for now ============
 
 
 // =========== start of exercises ============
 if (randomFloatingPointNumber < 33) {
-console.log("rock")
+var computersChoice = 'rock'
+// console.log("rock")
 } else if (randomFloatingPointNumber > 34 && randomFloatingPointNumber< 67){
-console.log("paper")
+var computersChoice = 'paper'
+// console.log("paper")
 }else if (randomFloatingPointNumber > 67) {
-console.log("scissors")
+var computersChoice = 'scissors'
+// console.log("scissors")
 }
 
 // exercise 1:
