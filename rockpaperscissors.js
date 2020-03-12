4af90707405d6d143997addf2d28007f26d75348
beneatspineapple <38222021+beneatspineapple@users.noreply.github.com>
2020-03-11 21:25:59 -0700

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,85 +1,97 @@
-function playNGamesOfRPS(numOfGame){for (let i = 0; i < numOfGame; i++) {playRPS();
-}}
+function playNGamesOfRPS(numOfGame){
+    for (let i = 0; i < numOfGame; i++) { //++ is increment operator (natural counting, add one)
+        playRPS();
+    }
+}
 
 function playRPS(){
 
 
-  /*
-   * "initial setting-things-up" section
-   */
-  // =========== leave the below section for now ============
-  // prompt the user for input, and store their response value into another variable
-  let userInputtedRPSString = prompt("type your choice: rock, paper, or scissors");
+    /*
+    * "initial setting-things-up" section
+    */
+    // =========== leave the below section for now ============
+    // prompt the user for input, and store their response value into another variable
+    let userInputtedRPSString = prompt("type your choice: rock, paper, or scissors");
 
-  // create a variable to store a random number from 1 to 100
-  let randomFloatingPointNumber = Math.random() * 100;
+    // create a variable to store a random number from 1 to 100
+    let randomFloatingPointNumber = Math.random() * 100;
 
-  // a variable to hold a string representing the computer opponents choice
-  let computersChoice = '';
-  // =========== leave above section for now ============
+    // a variable to hold a string representing the computer opponents choice
+    let computersChoice = '';
+    // =========== leave above section for now ============
 
-  /*
-   * "declare and initialize the variables we need" section:
-   */
-  let userWonTheGame = false;
-  let userTiedTheGame = false;
+    /*
+    * "declare and initialize the variables we need" section:
+    */
+    let userWonTheGame = false;
+    let userTiedTheGame = false;
 
-  /*
-   * "Computer's choice" section:
-   */
-  if (randomFloatingPointNumber < 33) {
-    computersChoice = 'rock';
-  }
-  else if (randomFloatingPointNumber > 34 && randomFloatingPointNumber < 67) {
-    computersChoice = 'paper';
-  }
-  else if (randomFloatingPointNumber > 67) {
-    computersChoice = 'scissors';
-  }
-
-  /*
-   * "see who won" section:
-   */
-  // computer chose "rock" case:
-  if (computersChoice == 'rock') {
-    if (userInputtedRPSString == 'rock') {
-      // console.log("tie");
-      userTiedTheGame = true;
+    /*
+    * "Computer's choice" section:
+    */
+    if
+    (
+        randomFloatingPointNumber < 33
+    ) {
+        computersChoice = 'rock';
     }
-    else if (userInputtedRPSString == 'paper') {
-      // console.log("user wins");
-      userWonTheGame = true;
+    else if
+    (
+        randomFloatingPointNumber > 34
+        &&
+        randomFloatingPointNumber < 67
+    ) {
+        computersChoice = 'paper';
     }
-  }
+    else if (randomFloatingPointNumber > 67) {
+        computersChoice = 'scissors';
+    }
 
-  // computer chose "scissors" case:
-  if (computersChoice == 'scissors') {
-    if (userInputtedRPSString == 'scissors') {
-      userTiedTheGame = true;
+    /*
+    * "see who won" section:
+    */
+    // computer chose "rock" case:
+    if (computersChoice == 'rock') { // this is called a boolean expression inside the parentheses because it always evaluates to true or false
+        if (userInputtedRPSString == 'rock') {
+            // console.log("tie");
+            userTiedTheGame = true;
+        }
+        else if (userInputtedRPSString == 'paper') {
+            // console.log("user wins");
+            userWonTheGame = true;
+        }
     }
-    else if (userInputtedRPSString == 'rock') {
-      // console.log("user wins");
-      userWonTheGame = true;
-    }
-  }
 
-  // computer chose "paper" case:
-  if (computersChoice == 'paper') {
-    if (userInputtedRPSString == 'paper') {
-      userTiedTheGame = true;
+    // computer chose "scissors" case:
+    if (computersChoice == 'scissors') {
+        if (userInputtedRPSString == 'scissors') {
+            userTiedTheGame = true;
+        }
+        else if (userInputtedRPSString == 'rock') {
+            // console.log("user wins");
+            userWonTheGame = true;
+        }
     }
-    else if (userInputtedRPSString == 'scissors') {
-      userWonTheGame = true;
-    }
-  }
 
-  if (userWonTheGame == true) {
-    console.log( "You won!: "+ userWonTheGame );
-  }
-  else if (userWonTheGame == false) {
-    console.log( "You lost!: "+ userWonTheGame );
-  }
-  if (userTiedTheGame == true) {
-    console.log( "Tie: "+ userTiedTheGame );
-  }
+    // computer chose "paper" case:
+    if (computersChoice == 'paper') {
+        if (userInputtedRPSString == 'paper') {
+            userTiedTheGame = true;
+        }
+        else if (userInputtedRPSString == 'scissors') {
+            userWonTheGame = true;
+        }
+    }
+
+    if (userWonTheGame == true) {
+        console.log( "You won!: "+ userWonTheGame );
+    }
+    else if (userWonTheGame == false) {
+        console.log( "You lost!: "+ userWonTheGame );
+    }
+
+    if (userTiedTheGame == true) {
+        console.log( "Tie: "+ userTiedTheGame );
+    }
 }
